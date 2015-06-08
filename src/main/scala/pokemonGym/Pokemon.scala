532f576ecb69033eed768ad301cb0875085798bd
lemora <Leandro.X.Mora.-ND@disney.com>
2015-06-08 17:59:20 -0300

    Primer commit

---
+++ src/main/scala/pokemonGym/Pokemon.scala
@@ -0,0 +1,20 @@
+package pokemonGym
+
+/**
+ * Created by leandromoras on 6/8/15.
+ */
+abstract class Pokemon {
+
+  val level: Int = 1;
+  val experience: Int = 0;
+  val energy: Int = 0;
+  val Genre: Char;
+  val maxEnergy: Int;
+  val weight: Int;
+  val strenght: Int;
+  val velocity: Int;
+ // val listPrimaryAttacks: List[Attack];
+ // val listSecundaryAttacks: List[Attack];
+
+}
+
